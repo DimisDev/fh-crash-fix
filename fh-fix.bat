baf6a537b57c923668816b1fb1c83ecc110c83e4
Dimis <94694396+DimisSSH@users.noreply.github.com>
2023-02-25 07:08:46 +0000

    Add files via upload
    Forza Horizon 5 & 4 Fix Patch

---
+++ fh-fix.bat
@@ -0,0 +1,16 @@
+@echo off
+title Forza Horizon Crash Fix - @DimisSSH on github
+cls
+taskkill /f /im gamelaunchhelper.exe
+cls
+taskkill /f /im ForzaHorizon5.exe
+cls
+taskkill /f /im ForzaHorizon4.exe
+cls
+taskkill /f /im explorer.exe
+ping localhost >nul
+echo Enter the password for [limiteduser]
+runas /user:limiteduser "explorer.exe"
+cls
+echo Crash Patched, open your game!
+pause >nul
